74458a89709cad27d3b887585e9972f879f937e4
mdsacra <mdsacra@gmail.com>
2020-08-08 08:12:42 -0300

    Iniciando o projeto.

---
+++ centraldeerros/src/test/java/com/mdsacra/centraldeerros/CentraldeerrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdsacra.centraldeerros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CentraldeerrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
